55f585398a7cb1ad45a41d08bc9f3bc4f0d9ca5c
Sanket-Thorat99 <sanketthorat9935@gmail.com>
2026-07-06 00:01:16 +0530

    Created Event Controller

---
+++ backend/controllers/eventController.js
@@ -0,0 +1,42 @@
+const Event = require("../models/Event");
+
+const createEvent = async (req, res) => {
+    try {
+
+        const {
+            title,
+            description,
+            category,
+            date,
+            time,
+            venue,
+            maxParticipants
+        } = req.body;
+
+        const event = await Event.create({
+            title,
+            description,
+            category,
+            date,
+            time,
+            venue,
+            maxParticipants,
+
+            organizer: req.user._id,
+        });
+
+        res.status(201).json({
+            message: "Event Created Successfully",
+            event,
+        });
+
+    } catch(error) {
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+};
+
+module.exports = {
+    createEvent,
+};
