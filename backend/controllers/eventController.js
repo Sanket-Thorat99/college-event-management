dc840f0a15888d7b169225fdf18c3dea6bdbb95f
Sanket-Thorat99 <sanketthorat9935@gmail.com>
2026-07-15 00:12:57 +0530

    added getAllEvents and getEventById functions in eventController

--- backend/controllers/eventController.js
+++ backend/controllers/eventController.js
@@ -38,6 +38,49 @@ const createEvent = async (req, res) => {
     }
 };
 
+const getAllEvents = async (req, res) => {
+    try {
+
+        const events = await Event.find()
+            .populate("organizer", "name email")
+            .sort({ date: 1 });
+
+        res.status(200).json({
+            count: events.length,
+            events,
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+};
+
+const getEventById = async (req,res) => {
+    try{
+        const {id} = req.params;
+
+        const event = await Event.findById(id)
+            .populate("organizer","name email");
+
+        if(!event){
+            return res.status(404).json({
+                messsage: "Event not Found",
+            });
+        }
+
+        res.status(200).json(event);
+
+    }catch(error){
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+};
+
 module.exports = {
     createEvent,
+    getAllEvents,
+    getEventById,
 };
--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const eventRouter = express.Router();
 
-const {createEvent} = require("../controllers/eventController");
+const {createEvent,getAllEvents,getEventById} = require("../controllers/eventController");
 const { protect } = require("../middleware/authMiddleware");
 const { authorizeRoles } = require("../middleware/roleMiddleware");
+
+eventRouter.get("/", getAllEvents);
+eventRouter.get("/:id", getEventById);
 
 eventRouter.post("/",protect,
     authorizeRoles("organizer","admin"),createEvent
 );
+
 
 module.exports = eventRouter;
